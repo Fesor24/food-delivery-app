8bf04155d1ca3e123e6fa184ce089d28535397ce
Wale <onafesowale@gmail.com>
2023-06-06 21:36:32 -0700

    RestaurantSpecification

---
+++ Core/Specifications/RestaurantSpecification.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class RestaurantSpecification : BaseSpecification<Restaurant>
+    {
+        public RestaurantSpecification()
+        {
+
+        }
+    }
+}
