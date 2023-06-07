9b9b15a3cb97a911eb530ca8d3ae75b307be009d
Wale <onafesowale@gmail.com>
2023-06-06 21:36:42 -0700

    SpecificationEvaluator

---
+++ Core/Specifications/SpecificationEvaluator.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Specifications
+{
+    public class SpecificationEvaluator<TEntity> where TEntity: BaseEntity
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> query, ISpecification<TEntity> spec)
+        {
+            if(spec.Criteria is not null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+
+
+            return query;
+        }
+    }
+}
