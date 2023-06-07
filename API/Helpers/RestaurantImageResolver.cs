4ce5da9ebcae0ece811e15344331459a3779af2f
Wale <onafesowale@gmail.com>
2023-06-06 21:34:21 -0700

    RestaurantImageResolver

---
+++ API/Helpers/RestaurantImageResolver.cs
@@ -0,0 +1,26 @@
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Helpers
+{
+    public class RestaurantImageResolver : IValueResolver<Restaurant, RestaurantDto, string>
+    {
+        private readonly IConfiguration _config;
+
+        public RestaurantImageResolver(IConfiguration config)
+        {
+            _config= config;
+        }
+
+        public string Resolve(Restaurant source, RestaurantDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return _config["Host"] + source.PictureUrl;
+            }
+
+            return null;
+        }
+    }
+}
