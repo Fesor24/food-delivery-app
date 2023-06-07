4c35a488e98f51384de589eabb31df7b134ce745
Wale <onafesowale@gmail.com>
2023-06-06 21:36:22 -0700

    ISpecification

---
+++ Core/Specifications/ISpecification.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public interface ISpecification<T>
+    {
+        public Expression<Func<T, bool>> Criteria { get; }
+
+        public List<Expression<Func<T, object>>> Includes { get; }
+
+     
+    }
+}
