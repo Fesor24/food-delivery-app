417fed83a6c7b7f5875f1cafdbe9ff4f3da2af94
Wale <onafesowale@gmail.com>
2023-06-06 21:33:47 -0700

    Restaurant dto

---
+++ API/Dtos/RestaurantDto.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Dtos
+{
+    public class RestaurantDto
+    {
+        public string Name { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string Address { get; set; }
+
+        public float DeliveryFee { get; set; }
+    }
+}
