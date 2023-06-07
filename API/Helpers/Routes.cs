17ede2b5e3ea5619e58cb36b7026ed910d569d20
Wale <onafesowale@gmail.com>
2023-06-06 21:34:29 -0700

    Routes

---
+++ API/Helpers/Routes.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Helpers
+{
+    public class Routes
+    {
+        public const string AddRestaurants = "api/restaurants";
+
+        public const string FetchRestaurants = "api/restaurants";
+    }
+}
