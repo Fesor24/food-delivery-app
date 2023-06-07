d413b104d523f8246f4c8d34df9ca886a8487c73
Wale <onafesowale@gmail.com>
2023-06-06 21:33:17 -0700

    Error controller

---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using API.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    /// <summary>
+    /// Whatever path is in this route attribute must match that which we are passing to in the middleware
+    /// </summary>
+    [Route("errors/{code}")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorController : ControllerBase
+    {
+        public IActionResult Errors(int code)
+        {
+            return new ObjectResult(new ApiResponse { ErrorMessage = "Endpoint does not exist", ErrorResult = code });
+        }
+    }
+}
