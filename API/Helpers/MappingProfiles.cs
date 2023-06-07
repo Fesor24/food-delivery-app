b6a041df241e5513c46253a8c1c2c51ee9185ac7
Wale <onafesowale@gmail.com>
2023-06-06 21:34:08 -0700

    Mapping profiles

---
+++ API/Helpers/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Restaurant, RestaurantDto>()
+                .ForMember(x => x.PictureUrl, o => o.MapFrom<RestaurantImageResolver>())
+                .ReverseMap();
+        }
+    }
+}
