c8c94071a338277c43bdb234d58b5a45c26332e7
Wale <onafesowale@gmail.com>
2023-06-06 21:36:52 -0700

    ApplicationDbContext

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach(var change in ChangeTracker.Entries<BaseEntity>())
+            {
+                change.Entity.DateModified = DateTimeOffset.UtcNow;
+
+                if(change.State == EntityState.Added)
+                {
+                    change.Entity.DateCreated = DateTimeOffset.UtcNow;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public DbSet<Products> Products { get; set; }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
