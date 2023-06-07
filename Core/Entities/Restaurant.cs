6cfbaa380981fed27e4ea7352eae4c278ef455c8
Wale <onafesowale@gmail.com>
2023-06-06 21:35:48 -0700

    Restaurant class

---
+++ Core/Entities/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Entities
+{
+    public class Restaurant : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string Address { get; set; }
+
+        public float DeliveryFee { get; set; }
+        
+        public List<Products> Products { get; set; }
+    }
+}
