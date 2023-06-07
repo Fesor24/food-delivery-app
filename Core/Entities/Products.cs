22542bb89aee72b4765062390afdd4522c4e05a6
Wale <onafesowale@gmail.com>
2023-06-06 21:35:36 -0700

    Products class

---
+++ Core/Entities/Products.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Entities
+{
+    public class Products : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string RestaurantId { get; set; }
+
+        [ForeignKey(nameof(RestaurantId))]
+        public Restaurant Restaurant { get; set; }
+    }
+}
