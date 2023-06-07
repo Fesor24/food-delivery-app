90e27abc0d0ffbe99558f32bac72245918cde139
Wale <onafesowale@gmail.com>
2023-06-06 21:33:27 -0700

    Product controller

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+﻿using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public ProductController()
+        {
+
+        }
+    }
+}
