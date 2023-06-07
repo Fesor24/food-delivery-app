63ca78eb964477f6569e063d9761327a23b6d261
Wale <onafesowale@gmail.com>
2023-06-06 21:35:57 -0700

    IGenericRepository

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using Core.Specifications;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T: BaseEntity
+    {
+        Task<T> GetAsync(string id);
+
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        Task<T> GetWithSpecAsync(ISpecification<T> spec);
+
+        Task<IReadOnlyList<T>> GetAllWIthSpecAsync(ISpecification<T> spec);
+
+        Task AddListAsync(List<T> entities);
+    }
+}
