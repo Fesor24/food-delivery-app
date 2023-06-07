25a7184e46f36ae96bddaf631f8cf05c2d1eda2c
Wale <onafesowale@gmail.com>
2023-06-06 21:34:55 -0700

    ApiResponse

---
+++ API/Response/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace API.Response
+{
+    public class ApiResponse
+    {
+        public bool Successful;
+
+        public string ErrorMessage { get; set; }
+
+        public object WarningResult { get; set; }
+
+        public object ErrorResult { get; set; } 
+
+        public object Result { get; set; }
+        
+
+    }
+
+    public class ApiResponse<TResult, TWarningResult, TErrorResult> : ApiResponse
+    {
+        public new TResult Result { get => (TResult)base.Result; set => base.Result = value; }
+
+        public new TWarningResult WarningResult { get => (TWarningResult)base.WarningResult; set => base.WarningResult = value; }
+
+        public new TErrorResult ErrorResult { get => (TErrorResult)base.ErrorResult; set => base.ErrorResult = value; }
+    }
+}
