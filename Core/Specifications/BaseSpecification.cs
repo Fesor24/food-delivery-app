bf81e1d9cb3702c09cb5218c96577504e57d9132
Wale <onafesowale@gmail.com>
2023-06-12 09:47:15 -0700

    Added filtering and ordering functionalities

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -18,9 +18,23 @@ namespace Core.Specifications
 
         public List<Expression<Func<T, object>>> Includes { get; } = new();
 
+        public Expression<Func<T, object>> OrderByExp { get; private set; }
+
+        public Expression<Func<T, object>> OrderByDescExp { get; private set; }
+
         protected void AddIncludes(Expression<Func<T, object>> includes)
         {
             Includes.Add(includes);
+        }
+
+        protected void AddOrderByExp(Expression<Func<T, object>> orderByExp)
+        {
+            OrderByExp = orderByExp;
+        }
+
+        protected void AddOrderByDescExp(Expression<Func<T, object>> orderByDescExp)
+        {
+            OrderByDescExp = orderByDescExp;
         }
     }
 }
