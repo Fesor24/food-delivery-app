0db949f64ca2c84515ceb276566625cac85be022
Wale <onafesowale@gmail.com>
2023-06-12 09:47:47 -0700

    Ordering, filtering

--- Core/Specifications/RestaurantSpecification.cs
+++ Core/Specifications/RestaurantSpecification.cs
@@ -5,9 +5,24 @@ namespace Core.Specifications
 {
     public class RestaurantSpecification : BaseSpecification<Restaurant>
     {
-        public RestaurantSpecification()
+        public RestaurantSpecification(string sort, string location = null)
+            : base(x => string.IsNullOrWhiteSpace(location) || x.Address.ToLower().Contains(location.ToLower()))
         {
-
+            if (!string.IsNullOrWhiteSpace(sort))
+            {
+                switch (sort)
+                {
+                    case "priceDesc":
+                        AddOrderByDescExp(x => x.DeliveryFee);
+                        break;
+                    case "priceAsc":
+                        AddOrderByExp(x => x.DeliveryFee);
+                        break;
+                    default:
+                        AddOrderByExp(x => x.Name);
+                        break;
+                }
+            }     
         }
     }
 }
