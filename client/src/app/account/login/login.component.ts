58e4f1279f2206e3c83d3dac9e911524626c0ea9
Wale <onafesowale@gmail.com>
2023-07-21 22:36:05 +0100

    Reusable text input component

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ export class LoginComponent implements OnInit{
 
   loginForm!: FormGroup;
 
-  constructor(private accountService: AccountService){}
+  constructor(private accountService: AccountService, private router: Router){}
 
   ngOnInit(): void {
     this.createLoginForm()
@@ -19,15 +20,19 @@ export class LoginComponent implements OnInit{
 
   createLoginForm(){
     this.loginForm = new FormGroup({
-      email: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required)
-    })
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]
+      ),
+      password: new FormControl('', Validators.required),
+    });
   }
 
   onSubmit(){
     this.accountService.login(this.loginForm.value).subscribe((response) => {
+      this.router.navigateByUrl('/');
       console.log("User logged in");
-    })
+    }, error => console.log(error));
   }
 
 }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RestaurantCircComponent } from './components/restaurant-circ/restaurant-circ.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TextInputComponent } from './components/text-input/text-input.component';
 
 
 
 
 @NgModule({
   declarations: [
-    RestaurantCircComponent
+    RestaurantCircComponent,
+    TextInputComponent
   ],
   imports: [
     CommonModule,
@@ -16,7 +18,8 @@ import { ReactiveFormsModule } from '@angular/forms';
   ],
   exports : [
     RestaurantCircComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TextInputComponent
   ]
 })
 export class SharedModule { }
