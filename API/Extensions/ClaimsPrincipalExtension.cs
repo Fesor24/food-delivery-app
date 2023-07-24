835b39393164438e295717ca6c2f71aa973c0e79
Wale <onafesowale@gmail.com>
2023-07-24 05:37:40 +0100

    Claims Principal extension

---
+++ API/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace API.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string GetUsersEmailFromClaimsPrincipal(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.Email);
+        }
+    }
+}
