19b3018421597555e55be1c8de46827a578b72b8
Wale <onafesowale@gmail.com>
2023-07-24 05:37:09 +0100

    Order to return Dto

---
+++ API/Dtos/OrderToReturnDto.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities.OrderAggregate;
+
+namespace API.Dtos
+{
+    public class OrderToReturnDto
+    {
+        public string Id { get; set; }
+
+        public string CustomerEmail { get; set; }
+
+        public Address DeliveryAddress { get; set; }
+
+        public float SubTotal { get; set; }
+
+        public float DeliveryCharges { get; set; }
+
+        public IReadOnlyList<OrderItemDto> OrderItems { get; set; }
+
+        public string Status { get; set; }
+
+        public float Total { get; set; }
+
+        public string DateCreated { get; set; }
+    }
+}
