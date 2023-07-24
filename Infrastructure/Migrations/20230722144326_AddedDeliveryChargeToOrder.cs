69614aa2cfc3a0d85b79551ade7356f945de1644
Wale <onafesowale@gmail.com>
2023-07-24 05:39:05 +0100

    Migration files

---
+++ Infrastructure/Migrations/20230722144326_AddedDeliveryChargeToOrder.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class AddedDeliveryChargeToOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "DeliveryCharges",
+                table: "Orders",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryCharges",
+                table: "Orders");
+        }
+    }
+}
