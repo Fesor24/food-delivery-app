86a2d3114ea527f682f74534ef8efdeb54ed17ee
Wale <onafesowale@gmail.com>
2023-07-24 05:40:03 +0100

    Specifications

---
+++ Core/Specifications/OrderByIdAndEmailSpecification.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities.OrderAggregate;
+
+namespace Core.Specifications
+{
+    public class OrderByIdAndEmailSpecification : BaseSpecification<Order>
+    {
+        public OrderByIdAndEmailSpecification(string email, string id): base(x => x.Id == id && x.CustomerEmail == email)
+        {
+            AddIncludes(x => x.OrderItems);
+        }
+    }
+}
---
+++ Core/Specifications/OrdersForUserSpecification.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities.OrderAggregate;
+
+namespace Core.Specifications
+{
+    public class OrdersForUserSpecification : BaseSpecification<Order>
+    {
+        public OrdersForUserSpecification(string email) : base(x => x.CustomerEmail == email)
+        {
+            AddIncludes(x => x.OrderItems);
+            AddOrderByDescExp(x => x.DateCreated);
+        }
+    }
+}
