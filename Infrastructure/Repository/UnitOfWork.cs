4096b567d804c93d8be2c0ed3a933f170018e33c
Wale <onafesowale@gmail.com>
2023-07-24 05:39:36 +0100

    Unit of work

---
+++ Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+
+namespace Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        private Hashtable _repositories;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
+        {
+            if(_repositories is null) _repositories = new Hashtable();
+
+            var key = typeof(TEntity).Name;
+
+            if (!_repositories.ContainsKey(key))
+            {
+                var repositoryType = typeof(GenericRepository<>);
+
+                var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(typeof(TEntity)), _context);
+
+                _repositories.Add(key, repositoryInstance);
+            }
+
+            return (IGenericRepository<TEntity>)_repositories[key];
+        }
+    }
+}
