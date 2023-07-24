a753313556ebd096d5b3f5f00d1f51fdf9772f21
Wale <onafesowale@gmail.com>
2023-07-24 05:38:26 +0100

    OrderImageResolver

---
+++ API/Helpers/OrderImageResolver.cs
@@ -0,0 +1,26 @@
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities.OrderAggregate;
+
+namespace API.Helpers
+{
+    public class OrderImageResolver : IValueResolver<OrderItem, OrderItemDto, string>
+    {
+        private readonly IConfiguration _config;
+
+        public OrderImageResolver(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string Resolve(OrderItem source, OrderItemDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrWhiteSpace(source.ItemOrdered.PictureUrl))
+            {
+                return _config["Host"] + source.ItemOrdered.PictureUrl;
+            }
+
+            return null;
+        }
+    }
+}
