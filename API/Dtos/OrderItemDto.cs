5452e4d3025ea0732e584d3057d989d230c567fd
Wale <onafesowale@gmail.com>
2023-07-24 05:36:57 +0100

    Order Item Dto

---
+++ API/Dtos/OrderItemDto.cs
@@ -0,0 +1,15 @@
+﻿namespace API.Dtos
+{
+    public class OrderItemDto
+    {
+        public int Quantity { get; set; }
+
+        public float Price { get; set; }
+
+        public string ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string PictureUrl { get; set; }
+    }
+}
