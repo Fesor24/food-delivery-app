5469798e9150757d84b7ac20b6953aed70fd8072
Wale <onafesowale@gmail.com>
2023-07-25 17:35:02 +0100

    Restaurant by id specification

---
+++ Core/Specifications/RestaurantByIdSpecification.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class RestaurantByIdSpecification : BaseSpecification<Restaurant>
+    {
+        public RestaurantByIdSpecification(string restaurantId) : base(x =>  x.Id == restaurantId)
+        {
+
+        }
+    }
+}
