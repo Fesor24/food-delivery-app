83a9c7d2926551388e0ee348fa5f56231a8aabe9
Wale <onafesowale@gmail.com>
2023-07-26 04:14:59 +0100

    payment status enum

---
+++ Core/Entities/OrderAggregate/PaymentStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace Core.Entities.OrderAggregate
+{
+    public enum PaymentStatus
+    {
+        [EnumMember(Value ="Pending")]
+        Pending,
+
+        [EnumMember(Value = "Successful")]
+        Successful,
+
+        [EnumMember(Value = "Failed")]
+        Failed
+    }
+}
