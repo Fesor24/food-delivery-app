34cda80806ad4b7f1b68ec61dabd876502d63c81
Wale <onafesowale@gmail.com>
2023-07-26 04:20:06 +0100

    Payment result model

---
+++ Core/Models/PaymentResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class PaymentResult
+    {
+        public string ErrorMessage { get; set; }
+
+        public bool Successful => ErrorMessage == null;
+
+        public object Result { get; set; }
+
+        public object Error { get; set; }
+    }
+
+    public class PaymentResult<TResult, TError>: PaymentResult
+    {
+        public new TResult Result { get => (TResult)base.Result; set => base.Result = value; }
+
+        public new TError Error { get => (TError)base.Error; set => base.Error = value; }
+    }
+}
