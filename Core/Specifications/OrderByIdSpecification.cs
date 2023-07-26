2c44561c701fbe7cab3b5771998fd19583ea49ef
Wale <onafesowale@gmail.com>
2023-07-26 04:15:39 +0100

    OrderByIdSpecification

---
+++ Core/Specifications/OrderByIdSpecification.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Entities.OrderAggregate;
+
+namespace Core.Specifications
+{
+    public class OrderByIdSpecification : BaseSpecification<Order>
+    {
+        public OrderByIdSpecification(string orderId) : base(x => x.Id == orderId)
+        {
+            AddIncludes(x => x.OrderItems);
+        }
+    }
+}
