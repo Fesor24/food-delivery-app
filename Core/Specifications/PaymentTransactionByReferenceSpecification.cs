faf2372a250d33730867cdcf114273e133b454f0
Wale <onafesowale@gmail.com>
2023-07-26 04:16:00 +0100

    Payment transaction by reference specification

---
+++ Core/Specifications/PaymentTransactionByReferenceSpecification.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class PaymentTransactionByReferenceSpecification : BaseSpecification<PaymentTransaction>
+    {
+        public PaymentTransactionByReferenceSpecification(string trxref) : base(x => x.Reference == trxref)
+        {
+
+        }
+    }
+}
