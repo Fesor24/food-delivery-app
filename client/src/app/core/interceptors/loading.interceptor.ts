39d337a46bede1f09bdd73a5f031ec6aefb141c4
Wale <onafesowale@gmail.com>
2023-07-26 04:19:54 +0100

    Modifed the delay

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -14,15 +14,16 @@ export class LoadingInterceptor implements HttpInterceptor {
   constructor(private busyService: BusyService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if(!request.url.includes("email-exists")){
+    if (
+      !request.url.includes('email-exists')) {
       this.busyService.busy();
     }
 
-    return next.handle(request).pipe(
-      delay(700),
-      finalize(() => {
-        this.busyService.idle();
-      })
-    );
+      return next.handle(request).pipe(
+        delay(500),
+        finalize(() => {
+          this.busyService.idle();
+        })
+      );
   }
 }
