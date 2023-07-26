b87e1fe78e0d3152ca5d2def2f686e3d4ea53e8d
Wale <onafesowale@gmail.com>
2023-07-26 04:18:00 +0100

    Payment transaction entity

---
+++ Core/Entities/PaymentTransaction.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Entities
+{
+    public class PaymentTransaction : BaseEntity
+    {
+        public string UserEmail { get; set; }
+
+        public string OrderId { get; set; }
+
+        public bool Verified { get; set; } = false;
+
+        public string Reference { get; set; }
+
+        public float Amount { get; set; }
+    }
+}
