3d0daf0d45dd32dc2fb1bade5fc2bca13b9ace91
Wale <onafesowale@gmail.com>
2023-07-26 04:19:11 +0100

    Payment model

---
+++ Core/Models/Payment.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Models
+{
+    public class Payment
+    {
+        public float Amount { get; set; }
+
+        public string Email { get; set; }
+
+        public string Currency => "NGN";
+
+        public string CallbackUrl { get; set; }
+
+        public string OrderId { get; set; }
+    }
+}
