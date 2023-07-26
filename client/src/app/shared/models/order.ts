4cfaf0a6b3fe5bb14b358202d68826fed3b50cef
Wale <onafesowale@gmail.com>
2023-07-26 09:05:48 +0100

    Added IOrderSummary model

--- client/src/app/shared/models/order.ts
+++ client/src/app/shared/models/order.ts
@@ -1,3 +1,5 @@
+import { IShoppingCartItem } from "./shoppingCart";
+
 export interface IOrder{
   cartId: string;
   address: IAddress
@@ -10,4 +12,24 @@ export interface IAddress{
   street: string;
   city: string;
   state: string;
+}
+
+export interface ISummaryOrder {
+  id: string;
+  customerEmail: string;
+  dateCreated: string;
+  deliveryCharges: number;
+  paymentStatus: string;
+  status: string;
+  subTotal: number;
+  total: number;
+  deliveryAddress: IAddress;
+  orderItems: IOrderSummaryItem[];
+}
+
+export interface IOrderSummaryItem{
+  productName: string;
+  pictureUrl: string;
+  quantity: number;
+  price:number;
 }
