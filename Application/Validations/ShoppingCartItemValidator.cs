c130be1ed3ab7ed54baf2fec64d52421638f4ef3
Wale <onafesowale@gmail.com>
2023-08-05 12:34:26 +0100

    ShoppingCartItemValidator

---
+++ Application/Validations/ShoppingCartItemValidator.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dtos;
+using FluentValidation;
+
+namespace Application.Validations;
+public class ShoppingCartItemValidator : AbstractValidator<ShoppingCartItemDto>
+{
+	public ShoppingCartItemValidator()
+	{
+		RuleFor(x => Convert.ToDouble(x.Price)).GreaterThan(1.0).WithMessage("Price can not be less than 1");
+
+		RuleFor(x => x.Quantity).GreaterThanOrEqualTo(1).WithMessage("Quantity can not be less than 1");
+
+		RuleFor(x => x.Name).NotEmpty().NotNull().WithMessage("Name can not be null or empty");
+
+		RuleFor(x => x.Id).NotNull().NotEmpty().WithMessage("Id can not be null or empty");
+	}
+}
