8b15a60a27a28e220fb0c72aeb2ab07ee42a6a28
Wale <onafesowale@gmail.com>
2023-08-05 12:34:46 +0100

    RegisterValidator

---
+++ Application/Validations/RegisterValidator.cs
@@ -0,0 +1,17 @@
+﻿using Application.Dtos;
+using FluentValidation;
+
+namespace Application.Validations;
+internal class RegisterValidator : AbstractValidator<RegisterDto>
+{
+	public RegisterValidator()
+	{
+        RuleFor(x => x.Email).NotNull().NotEmpty().WithMessage("Email can not be null or empty");
+
+        RuleFor(x => x.Email).EmailAddress().WithMessage("Specify a valid email address");
+
+        RuleFor(x => x.Password).NotEmpty().NotNull().WithMessage("Password can not be null or empty");
+
+        RuleFor(x => x.Password).MinimumLength(5).WithMessage("Password must be at least 5 characters");
+    }
+}
