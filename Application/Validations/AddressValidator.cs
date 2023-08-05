8a7750f6645571cff42801a77987ff01c15147c9
Wale <onafesowale@gmail.com>
2023-08-05 12:33:44 +0100

    Address fluent validation

---
+++ Application/Validations/AddressValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application.Dtos;
+using FluentValidation;
+
+namespace Application.Validations;
+public class AddressValidator : AbstractValidator<AddressDto>
+{
+	public AddressValidator()
+	{
+		RuleFor(x => x.FirstName).NotNull().NotEmpty().WithMessage("First name can not be null or empty");
+
+        RuleFor(x => x.LastName).NotNull().NotEmpty().WithMessage("Last name can not be null or empty");
+
+        RuleFor(x => x.Street).NotNull().NotEmpty().WithMessage("Street can not be null or empty");
+
+        RuleFor(x => x.State).NotNull().NotEmpty().WithMessage("State name can not be null or empty");
+
+        RuleFor(x => x.City).NotNull().NotEmpty().WithMessage("City name can not be null or empty");
+    }
+}
