617a5fae0170d41714cdb0e00196a926a4b0fe0f
Wale <onafesowale@gmail.com>
2023-08-05 12:33:59 +0100

    create product fluent validation

---
+++ Application/Validations/CreateProductValidation.cs
@@ -0,0 +1,15 @@
+﻿using Application.Dtos;
+using FluentValidation;
+
+namespace Application.Validations;
+public class CreateProductValidation : AbstractValidator<CreateProductDto>
+{
+	public CreateProductValidation()
+	{
+		RuleFor(x => Convert.ToDouble(x.Price)).GreaterThan(1.0).WithMessage("Price can not be less than 1");
+
+		RuleFor(x => x.Name).NotEmpty().NotNull().WithMessage("Name can not be empty or null");
+
+		RuleFor(x => x.RestaurantId).NotNull().NotEmpty().WithMessage("Restaurant can not be null or empty");
+	}
+}
